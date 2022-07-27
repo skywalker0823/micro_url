a5670a7e978f82b03f01883f9bdcd3befc11d44e
pikxl <skywalker0823@hotmail.com>
2022-07-27 14:54:11 +0800

    dockerfile 1st edition complete.

--- app.py
+++ app.py
@@ -4,24 +4,30 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello World"
+
+
 @app.route("/micro",methods=["GET"])
 def get_req():
     return {"ok":"GET"}
 
 
 @app.route("/micro", methods=["POST"])
-def get_req():
+def post_req():
     return {"ok": "POST"}
 
 
 @app.route("/micro", methods=["PATCH"])
-def get_req():
+def patch_req():
     return {"ok": "PATCH"}
 
 
 @app.route("/micro", methods=["DELETE"])
-def get_req():
+def del_req():
     return {"ok": "DELETE"}
 
 
-app.run(port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
